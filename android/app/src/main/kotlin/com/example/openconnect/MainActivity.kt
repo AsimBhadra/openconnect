dfc44bc778dd75d97876487928acbbb7fafcb402
AsimBhadra <ashim@buildapps.dev>
2025-04-16 10:02:14 +0545

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/openconnect/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.openconnect
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ ios/openconnect_extension/PacketTunnelProvider.h
@@ -0,0 +1,12 @@
+//
+//  PacketTunnelProvider.h
+//  openconnect_extension
+//
+//  Created by Asim Bhadra on 16/04/2025.
+//
+
+#import <NetworkExtension/NetworkExtension.h>
+
+@interface PacketTunnelProvider : NEPacketTunnelProvider
+
+@end
